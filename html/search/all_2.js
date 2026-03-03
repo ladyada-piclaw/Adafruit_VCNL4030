9d209d2dc59bc011ce6581afb33c04e32d084f2e
Doxygen CI <ci-arduino@invalid>
2026-03-03 22:29:56 +0000

    Deploy docs to GitHub Pages from commit 637c9d30c6
    Commit: 637c9d30c6f5bc900a16ca8cb62f069747046338
    GitHub Actions run: 22645583924

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['enableactiveforcemode',['enableActiveForceMode',['../class_adafruit___v_c_n_l4030.html#a608dc20cc5cbc2c3cd2bdca9949fbc34',1,'Adafruit_VCNL4030']]],
-  ['enableals',['enableALS',['../class_adafruit___v_c_n_l4030.html#ab8539925e76a12de265c1c7d34495762',1,'Adafruit_VCNL4030']]],
-  ['enablealsinterrupt',['enableALSInterrupt',['../class_adafruit___v_c_n_l4030.html#acac9dea846dd230e1b0862473493bc74',1,'Adafruit_VCNL4030']]],
-  ['enableprox',['enableProx',['../class_adafruit___v_c_n_l4030.html#a0820bf8c3378e45a3d8a61b96afdf9d6',1,'Adafruit_VCNL4030']]],
-  ['enableproxlogicmode',['enableProxLogicMode',['../class_adafruit___v_c_n_l4030.html#ac4109d8384c489f902a57d8a5c06f283',1,'Adafruit_VCNL4030']]],
-  ['enablesunlightcancellation',['enableSunlightCancellation',['../class_adafruit___v_c_n_l4030.html#a8667435dafc9699db3aee086b90822dd',1,'Adafruit_VCNL4030']]],
-  ['enablewhitechannel',['enableWhiteChannel',['../class_adafruit___v_c_n_l4030.html#ab7efdb8ff7fa2dfb1c113e93542ab886',1,'Adafruit_VCNL4030']]]
+  ['enableactiveforcemode',['enableActiveForceMode',['../class_adafruit___v_c_n_l4030.html#aaa7541d05e041d2942ea9f7911428f86',1,'Adafruit_VCNL4030']]],
+  ['enableals',['enableALS',['../class_adafruit___v_c_n_l4030.html#abb446bbd199a0a211a852786b64849c0',1,'Adafruit_VCNL4030']]],
+  ['enablealsinterrupt',['enableALSInterrupt',['../class_adafruit___v_c_n_l4030.html#acdcab2941ef50f37d194c028286de8fa',1,'Adafruit_VCNL4030']]],
+  ['enableprox',['enableProx',['../class_adafruit___v_c_n_l4030.html#a011f6173ace98fee6250292f27d1b432',1,'Adafruit_VCNL4030']]],
+  ['enableproxlogicmode',['enableProxLogicMode',['../class_adafruit___v_c_n_l4030.html#a1554305650ed877a4aa5c45eac27a3ce',1,'Adafruit_VCNL4030']]],
+  ['enablesunlightcancellation',['enableSunlightCancellation',['../class_adafruit___v_c_n_l4030.html#af88c9955511b39549bd47f7663af69bf',1,'Adafruit_VCNL4030']]],
+  ['enablewhitechannel',['enableWhiteChannel',['../class_adafruit___v_c_n_l4030.html#aa922ff4f085382f770b5b5f60da2f94c',1,'Adafruit_VCNL4030']]]
 ];
--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['triggerproxreading',['triggerProxReading',['../class_adafruit___v_c_n_l4030.html#a8f36af135a9021678539f9751713b856',1,'Adafruit_VCNL4030']]]
+  ['triggerproxreading',['triggerProxReading',['../class_adafruit___v_c_n_l4030.html#a04973db888320e5248b6674877624445',1,'Adafruit_VCNL4030']]]
 ];
