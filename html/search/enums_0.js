53def26dcfdbbdc06777a8d4599a5d3f0ac82eac
Doxygen CI <ci-arduino@invalid>
2026-03-03 16:25:35 +0000

    Deploy docs to GitHub Pages from commit 424a87af82
    Commit: 424a87af82ab09730f1d141a9241cbf571c114cb
    GitHub Actions run: 22632269063

--- html/search/enums_0.js
+++ html/search/enums_0.js
@@ -2,11 +2,11 @@ var searchData=
 [
   ['vcnl4030_5fals_5fit_5ft',['vcnl4030_als_it_t',['../_adafruit___v_c_n_l4030_8h.html#a3bba1bbbeb96d7c2f455ee313e55052f',1,'Adafruit_VCNL4030.h']]],
   ['vcnl4030_5fals_5fpers_5ft',['vcnl4030_als_pers_t',['../_adafruit___v_c_n_l4030_8h.html#a251e2321e347a83b254bcf73d079a7d3',1,'Adafruit_VCNL4030.h']]],
-  ['vcnl4030_5fled_5fi_5ft',['vcnl4030_led_i_t',['../_adafruit___v_c_n_l4030_8h.html#a59abe3b59154f14ccd2b47d23cdab547',1,'Adafruit_VCNL4030.h']]],
-  ['vcnl4030_5fps_5fduty_5ft',['vcnl4030_ps_duty_t',['../_adafruit___v_c_n_l4030_8h.html#ade1651fe6b7c08fe583b44da3852a173',1,'Adafruit_VCNL4030.h']]],
-  ['vcnl4030_5fps_5fgain_5ft',['vcnl4030_ps_gain_t',['../_adafruit___v_c_n_l4030_8h.html#abd23c8833fb02af965047726c7b22fcb',1,'Adafruit_VCNL4030.h']]],
-  ['vcnl4030_5fps_5fint_5ft',['vcnl4030_ps_int_t',['../_adafruit___v_c_n_l4030_8h.html#affad3d1f48eb34a28a7047987be23e25',1,'Adafruit_VCNL4030.h']]],
-  ['vcnl4030_5fps_5fit_5ft',['vcnl4030_ps_it_t',['../_adafruit___v_c_n_l4030_8h.html#abc94ca787ff3465d0457a682c1cee67b',1,'Adafruit_VCNL4030.h']]],
-  ['vcnl4030_5fps_5fpers_5ft',['vcnl4030_ps_pers_t',['../_adafruit___v_c_n_l4030_8h.html#a4fdc1011254cb2d275e7ee3930826c95',1,'Adafruit_VCNL4030.h']]],
-  ['vcnl4030_5fps_5fsc_5fcur_5ft',['vcnl4030_ps_sc_cur_t',['../_adafruit___v_c_n_l4030_8h.html#a5c8a7bbc7e376621c78d8ae4f6a20cd5',1,'Adafruit_VCNL4030.h']]]
+  ['vcnl4030_5fprox_5fduty_5ft',['vcnl4030_prox_duty_t',['../_adafruit___v_c_n_l4030_8h.html#a24d47653642da95bd90edf804deabecf',1,'Adafruit_VCNL4030.h']]],
+  ['vcnl4030_5fprox_5fgain_5ft',['vcnl4030_prox_gain_t',['../_adafruit___v_c_n_l4030_8h.html#a2b8a731710242e885c65eb06a92798c0',1,'Adafruit_VCNL4030.h']]],
+  ['vcnl4030_5fprox_5fint_5ft',['vcnl4030_prox_int_t',['../_adafruit___v_c_n_l4030_8h.html#a590435c20cdb74251d280c1c475b2691',1,'Adafruit_VCNL4030.h']]],
+  ['vcnl4030_5fprox_5fit_5ft',['vcnl4030_prox_it_t',['../_adafruit___v_c_n_l4030_8h.html#a6cfcfeadf6522c3eac124bb723bc5c24',1,'Adafruit_VCNL4030.h']]],
+  ['vcnl4030_5fprox_5fled_5ft',['vcnl4030_prox_led_t',['../_adafruit___v_c_n_l4030_8h.html#acce159a9d933b1068c460d5d6db35339',1,'Adafruit_VCNL4030.h']]],
+  ['vcnl4030_5fprox_5fpers_5ft',['vcnl4030_prox_pers_t',['../_adafruit___v_c_n_l4030_8h.html#a02e9db79008748e3d617d88f65992a53',1,'Adafruit_VCNL4030.h']]],
+  ['vcnl4030_5fprox_5fsc_5fcur_5ft',['vcnl4030_prox_sc_cur_t',['../_adafruit___v_c_n_l4030_8h.html#ab1a69944ca1055f4df65b262fe75c57d',1,'Adafruit_VCNL4030.h']]]
 ];
